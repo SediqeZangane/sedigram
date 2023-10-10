7a9fdb84af6c661f2c9c0301585070a3ded40ff6
sedighe zanganeh <se.zanganeh@yahoo.com>
2023-10-10 20:20:47 +0200

    PostDetailScreen UI Done

--- lib/post_detail/post_detail_screen.dart
+++ lib/post_detail/post_detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sedigram/core/domain/post.dart';
+import 'package:sedigram/user/application/global_user_bloc.dart';
+import 'package:sedigram/user/application/global_user_state.dart';
 
 class PostDetailScreen extends StatelessWidget {
   static const String routeNamed = 'postDetailScreen';
@@ -15,31 +18,69 @@ class PostDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Center(
+          child: Padding(
+            padding: EdgeInsets.only(right: 36),
+            child: Text(
+              'Posts',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+      ),
       body: ListView.builder(
         itemCount: posts.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: const [
-                    CircleAvatar(radius: 24),
-                    SizedBox(width: 10),
-                    Text('userName'),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8, left: 4),
+                child: Row(
+                  children: [
+                    const CircleAvatar(radius: 24),
+                    const SizedBox(width: 10),
+                    BlocBuilder<GlobalUserBloc, GlobalUserState>(
+                      builder: (context, state) {
+                        return Text(
+                          state.user.userName,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        );
+                      },
+                    )
                   ],
                 ),
-                CachedNetworkImage(
-                  imageUrl: posts[index].imageUrl,
-                  fit: BoxFit.fitWidth,
-                ),
-                Text(
+              ),
+              CachedNetworkImage(
+                imageUrl: posts[index].imageUrl,
+              ),
+              Row(
+                children: const [
+                  Icon(Icons.favorite_border),
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Icon(Icons.mode_comment_outlined),
+                  ),
+                  Icon(Icons.share)
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 4, bottom: 12),
+                child: Text(
                   posts[index].caption,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           );
         },
       ),
