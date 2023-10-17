0663996054051baa84bee01ce15109756003deca
sedighe zanganeh <se.zanganeh@yahoo.com>
2023-10-17 22:30:02 +0200

    add RefreshIndicator in TimelineScreen

--- lib/timeline_screen/presentation/timeline_screen.dart
+++ lib/timeline_screen/presentation/timeline_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sedigram/core/presentation/widget/post_widget.dart';
 import 'package:sedigram/timeline_screen/application/timeline_bloc.dart';
+import 'package:sedigram/timeline_screen/application/timeline_event.dart';
 import 'package:sedigram/timeline_screen/application/timeline_state.dart';
-import 'package:shimmer/shimmer.dart';
 
 class TimelineScreen extends StatefulWidget {
   const TimelineScreen({super.key});
@@ -22,32 +22,25 @@ class _TimelineScreenState extends State<TimelineScreen> {
       ),
       body: BlocBuilder<TimelineBloc, TimelineState>(
         builder: (context, timelineState) {
-          return ListView.builder(
-            itemCount: timelineState.listPost.length,
-            itemBuilder: (context, index) {
-              if (timelineState.isLoading) {
-                return Center(child: shimmerWidget());
-              } else {
-                return PostWidget(
-                  isMine: timelineState.listPost[index].isMine,
-                  postDetailModel: timelineState.listPost[index],
-                );
-              }
+          return RefreshIndicator(
+            onRefresh: () async {
+              context.read<TimelineBloc>().add(TimelineInitEvent());
             },
+            child: timelineState.isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: timelineState.listPost.length,
+                    itemBuilder: (context, index) {
+                      return PostWidget(
+                        isMine: timelineState.listPost[index].isMine,
+                        postDetailModel: timelineState.listPost[index],
+                      );
+                    },
+                  ),
           );
         },
-      ),
-    );
-  }
-
-  Widget shimmerWidget() {
-    return Shimmer.fromColors(
-      baseColor: Colors.grey.shade300,
-      highlightColor: Colors.white,
-      child: Container(
-        height: 10,
-        width: 100,
-        color: Colors.green,
       ),
     );
   }
