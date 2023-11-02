7bbbb2a7331ade29eeb8ef701437d62317b7461b
sedighe zanganeh <se.zanganeh@yahoo.com>
2023-11-02 22:58:22 +0100

    use photo manager

--- lib/create_post/application/create_post_bloc.dart
+++ lib/create_post/application/create_post_bloc.dart
@@ -1,44 +1,50 @@
 // ignore_for_file: omit_local_variable_types
 
-import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_storage_path/flutter_storage_path.dart';
+import 'package:photo_manager/photo_manager.dart';
 import 'package:sedigram/create_post/application/create_post_event.dart';
 import 'package:sedigram/create_post/application/create_post_state.dart';
 import 'package:sedigram/create_post/domain/model/folder_path_model.dart';
 
 class CreatePostBloc extends Bloc<CreatePostEvent, CreatePostState> {
-  List<FolderPathModel>? list;
-
   CreatePostBloc() : super(CreatePostState.init()) {
     on<CreatePostEvent>((event, emit) async {
       if (event is GetImagesEvent) {
         emit(state.copyWith(gettingImages: true, folderList: []));
-        final imagePath = await StoragePath.imagesPath;
-        if (imagePath != null) {
-          final images = jsonDecode(imagePath) as List;
-          final pathFolders = images.map(
-            (dynamic e) {
-              // FolderPathModel.fromJson(
-              //   Map<String, dynamic>.from(e as Map),
-              // )
-              final map = e as Map;
-              final json = Map<String, dynamic>.from(map);
-              return FolderPathModel.fromJson(json);
+
+        await PhotoManager.requestPermissionExtend();
+        final folders = await PhotoManager.getAssetPathList(
+          type: RequestType.image,
+        );
+
+        final pathFolders = await Future.wait(
+          folders.sublist(0, min(folders.length, 4)).map(
+            (e) async {
+              final entities = await e.getAssetListPaged(page: 0, size: 200);
+              final files =
+                  await Future.wait(entities.map((en) async => en.file));
+              return FolderPathModel(
+                files: files
+                    .whereType<File>()
+                    .map((f) => f.absolute.path)
+                    .toList(),
+                folderName: e.name,
+              );
             },
-          ).toList();
-          emit(
-            state.copyWith(
-              gettingImages: false,
-              folderList: pathFolders,
-              folder: pathFolders.first,
-              selectedPath: pathFolders.first.files.first,
-            ),
-          );
-        } else {
-          emit(state.copyWith(gettingImages: false, folderList: []));
-        }
+          ),
+        );
+
+        emit(
+          state.copyWith(
+            gettingImages: false,
+            folderList: pathFolders,
+            folder: pathFolders.first,
+            selectedPath: pathFolders.first.files.first,
+          ),
+        );
       }
       if (event is ChangeFoldersEvent) {
         emit(
